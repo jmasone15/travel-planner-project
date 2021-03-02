0fe9c45c10bc9ffa8d0f110606d933b25df4dd98
Jordan Masone <jordanmasone1@gmail.com>
2021-03-02 11:53:41 -0500

    Added models folder for mongoose db.

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to the Mongo DB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/travelplanner",
+    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
 
 // Send every request to the React app
 // Define any API routes before this runs
