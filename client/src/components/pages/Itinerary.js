7f51ad64dd9a61490bedc4ccafca7b2e94b282c8
Jordan Masone <jordanmasone1@gmail.com>
2021-03-09 16:13:09 -0500

    Deploy

---
+++ client/src/components/pages/Itinerary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Itinerary() {
+    return (
+        <div>
+            <h1> This is the Itinerary page </h1>
+        </div>
+    )
+}
+
+export default Itinerary
