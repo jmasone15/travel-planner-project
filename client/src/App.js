9129fa902693d49f81f5e983e75b4e75a149deab
Jordan Masone <jordanmasone1@gmail.com>
2021-03-09 14:57:29 -0500

    Deployment.

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,10 @@ import Router from "./Router";
 import axios from "axios";
 import { AuthContextProvider } from "./context/AuthContext";
 import { UserContextProvider } from "./context/UserContext";
-import { CSSPlugin } from 'gsap/CSSPlugin';
-import gsap from "gsap";
 
 axios.defaults.withCredentials = true;
 
 function App() {
-  gsap.registerPlugin(CSSPlugin)
 
   return (
     <AuthContextProvider>
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import React from 'react';
 import { Link } from "react-router-dom";
-import { TimelineMax, Power1, CSSPlugin } from "gsap/all";
+import { gsap } from 'gsap'
+import { CSSPlugin } from 'gsap/CSSPlugin'
+import { TimelineMax, Power1 } from "gsap/all";
 // import donde from "../../images/dondeLogo.png";
 import Bgv from "../../components/Video/bgv.mp4";
 import "../Card/style.css";
@@ -49,7 +51,7 @@ class Login extends React.Component {
     };
 
     render() {
-        const plugins = [CSSPlugin];
+        gsap.registerPlugin(CSSPlugin)
         return (
             <div className="container h-100 mt-5">
                 <video
