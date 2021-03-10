044734075928e544226356079de602f157fa3cc3
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-03-10 11:56:16 -0600

    Added styling to activities area

--- client/src/components/Calendar/MyCard.js
+++ client/src/components/Calendar/MyCard.js
@@ -25,13 +25,13 @@ function MyCard(props) {
         <div>
             {props.data.map((place, index) => (
                 <div>
-                    <div className="row" style={{paddingBottom: "20px"}}>
+                    <div className="row" style={{paddingBottom: "20px", paddingTop: "20px"}}>
                     <div className="col-md-3">
                     {place.photos ?
                             <img  src={`https://maps.googleapis.com/maps/api/place/photo?photoreference=${place.photos[0].photo_reference}&sensor=false&maxheight=250&maxwidth=250&key=AIzaSyCoiYtN7Xjb7P4JIpWRtlMiL9uQirs_icI`} />
                             : null}
                     </div>
-                    <div className="col-md-8" style={{paddingLeft: "40px"}}>
+                    <div className="col-md-8" style={{paddingLeft: "90px"}}>
                     <h3>{place.name}</h3>
                     <h4>Rating: {place.rating}</h4> <p>Reviews: {place.user_ratings_total}</p>
                     <p>{place.formatted_address}</p>
