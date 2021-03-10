d0fffa5c2aa24609f0564378d2d77877e12ebd31
J.Laro <joselarojr@gmail.com>
2021-03-10 14:29:12 -0500

    refactored homePage added additional stylization/animation

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Card(props) {
     return (
-        <div ref={props.ref} style={props.style} className="card shadow p-3 mb-3` bg-white rounded">
+        <div ref={props.ref} style={props.style} className="card shadow p-3 mb-3 mt-5 bg-white rounded">
             <div className="card-body">{props.children}</div>
         </div>
     );
