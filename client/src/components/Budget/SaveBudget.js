49985b7a7d14298a1eefb94698bb8e08cba4b817
Deleon Francis <dxfrancis.coding@gmail.com>
2021-03-10 00:44:18 -0500

    initial restyle.

---
+++ client/src/components/Budget/SaveBudget.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SaveBudget({save}) {
+
+    return (
+        <button onClick={save}>
+        Save Budget to Trip
+      </button>
+    )
+}
+
+export default SaveBudget
