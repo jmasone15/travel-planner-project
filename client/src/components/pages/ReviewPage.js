864a9a42e82555cb810a9d8cb3a93c14ac8237de
Jordan Masone <jordanmasone1@gmail.com>
2021-03-10 18:30:43 -0500

    Added styling and functionality to Review Page.

--- client/src/components/pages/ReviewPage.js
+++ client/src/components/pages/ReviewPage.js
@@ -7,6 +7,16 @@ export default function ReviewPage(props) {
     const { userId } = useContext(UserContext);
     const history = useHistory();
 
+    function getDateRange(x) {
+        let result = x.map(date => date.name)
+        return (`${result[0]} - ${result[result.length - 1]}`)
+    }
+
+    function changePage(e) {
+        e.preventDefault();
+        history.push("/budget")
+    }
+
     async function saveTravelPlans(e) {
         e.preventDefault();
         console.log(props.tripExpenses);
@@ -18,7 +28,7 @@ export default function ReviewPage(props) {
                 expenses: JSON.parse(props.tripExpenses),
                 startLocation: props.tripStartLocation,
                 destination: props.tripDestination,
-                dates: JSON.parse(props.tripDates),
+                dates: props.tripDates,
                 userId: userId
             }
             await axios.post("/api/new", travelData);
@@ -37,15 +47,20 @@ export default function ReviewPage(props) {
     }
 
     return (
-        <div>
-            <h1>Review your trip</h1>
-            <h4>Trip name: {props.tripTripName}</h4>
-            <h4>Trip Budget: {props.tripBudget}</h4>
-            <h4>Trip Expenses: {props.tripExpenses}</h4>
-            <h4>Trip Start Location: {props.tripStartLocation}</h4>
-            <h4>Trip Destination: {props.tripDestination}</h4>
-            <h4>Trip Dates: {props.tripDates}</h4>
-            <button onClick={(e) => saveTravelPlans(e)}>Submit Trip!</button>
+        <div className="bgThis">
+            <br /><br />
+            <div className="container shadow bg-light p-5 mt-3 col-lg-10" style={{ width: "500px", marginTop: "50px" }}>
+                <h3><b>Your Trip to:</b> {props.tripDestination}</h3>
+                <br />
+                <p><b>Trip name:</b> {props.tripTripName}</p>
+                <p><b>Budget:</b> {props.tripBudget}</p>
+                <p><b>Start Location: </b> {props.tripStartLocation}</p>
+                <p><b>Dates:</b> {getDateRange(props.tripDates)}</p>
+                <br />
+                <button className="btn btn-block btn-success mt-2 p-2 shadow" onClick={(e) => saveTravelPlans(e)}>Save Trip!</button>
+                <button className="btn btn-block btn-danger mt-2 p-2 shadow" onClick={(e) => changePage(e)}>Restart Trip</button>
+            </div>
+            <br />
         </div>
     )
 }
