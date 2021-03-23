e78a4c9daa9deffcdf8b364bd77fa0c8f02a463d
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-03-23 13:08:50 -0500

    Changed api calls to the backend

--- routes/API.js
+++ routes/API.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+const router = require("express").Router();
+const auth = require("../middleware/auth");
 import GOOGLE_API_KEY from "./key";
 const BASEURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
 const Locate = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCoiYtN7Xjb7P4JIpWRtlMiL9uQirs_icI"
@@ -15,33 +17,96 @@ function formatQuery(query) {
     return newQuery
 }
 
-function getLocation() {
-    return axios.post(Locate);
-}
+router.post("/location", auth, async (req, res) => {
+    try {
+        res.json(await axios.post(Locate));
 
-function getUserCity(lat, lng) {
-    return axios.get(getCity + lat + "," + lng + "&locality" + "&key=" + GOOGLE_API_KEY);
-}
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
 
-function attractions(where) {
-    const query = formatQuery(where)
-    return axios.get(BASEURL + query + "+points+of+interest" + language + "&key=" +  GOOGLE_API_KEY);
-}
+router.get("/userCity", auth, async (req, res) => {
+    try {
+        res.json(await axios.get(getCity + req.lat + "," + req.lng + "&locality" + "&key=" + GOOGLE_API_KEY));
 
-function hotels(where) {
-    const query = formatQuery(where)
-    return axios.get(BASEURL + query + "+hotels" + language + "&key=" +  GOOGLE_API_KEY);
-}
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
 
-function shopping(where) {
-    const query = formatQuery(where)
-    return axios.get(BASEURL + query + "+shopping" + language + "&key=" +  GOOGLE_API_KEY);
-}
+router.get("/attractions", auth, async (req, res) => {
+    try {
+        const query = formatQuery(req.where)
+        res.json(await axios.get(BASEURL + query + "+points+of+interest" + language + "&key=" +  GOOGLE_API_KEY));
 
-function restaurants(where) {
-    const query = formatQuery(where)
-    return axios.get(BASEURL + query + "+restaurants" + language + "&key=" +  GOOGLE_API_KEY);
-}
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+router.get("/hotels", auth, async (req, res) => {
+    try {
+        const query = formatQuery(req.where)
+        res.json(await axios.get(BASEURL + query + "+hotels" + language + "&key=" +  GOOGLE_API_KEY));
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+router.get("/shopping", auth, async (req, res) => {
+    try {
+        const query = formatQuery(req.where)
+        res.json(await axios.get(BASEURL + query + "+shopping" + language + "&key=" +  GOOGLE_API_KEY));
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+router.get("/restaurants", auth, async (req, res) => {
+    try {
+        const query = formatQuery(req.where)
+        res.json(await axios.get(BASEURL + query + "+restaurants" + language + "&key=" +  GOOGLE_API_KEY));
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+// function getLocation() {
+//     return axios.post(Locate);
+// }
+
+// function getUserCity(lat, lng) {
+//     return axios.get(getCity + lat + "," + lng + "&locality" + "&key=" + GOOGLE_API_KEY);
+// }
+
+// function attractions(where) {
+//     const query = formatQuery(where)
+//     return axios.get(BASEURL + query + "+points+of+interest" + language + "&key=" +  GOOGLE_API_KEY);
+// }
+
+// function hotels(where) {
+//     const query = formatQuery(where)
+//     return axios.get(BASEURL + query + "+hotels" + language + "&key=" +  GOOGLE_API_KEY);
+// }
+
+// function shopping(where) {
+//     const query = formatQuery(where)
+//     return axios.get(BASEURL + query + "+shopping" + language + "&key=" +  GOOGLE_API_KEY);
+// }
+
+// function restaurants(where) {
+//     const query = formatQuery(where)
+//     return axios.get(BASEURL + query + "+restaurants" + language + "&key=" +  GOOGLE_API_KEY);
+// }
 
 // function hotels(where) {
 //     query = formatQuery(where)
@@ -54,4 +119,4 @@ function restaurants(where) {
 // }
 // }
 
-export { attractions, hotels, shopping, restaurants, getLocation, getUserCity };
+module.exports = router;
