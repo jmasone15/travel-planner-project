f4fbae697b06a6241ac095a2dadda7ee155aa9f5
Jordan Masone <jordanmasone1@gmail.com>
2021-03-28 21:13:03 -0400

    Fixed layout and image sizing on attractions.

--- client/src/components/pages/ProfilePage.js
+++ client/src/components/pages/ProfilePage.js
@@ -87,15 +87,15 @@ export default function ProfilePage(props) {
             // backgroundSize: "cover",
             position: "relative",
             height: "100vh",
-            height:" 100%",
+            height: " 100%",
             width: "100%"
         }}>
             <div style={{ textAlign: "center" }}>
                 <h1>Welcome, {profileEmail}</h1>
-                <button style={{backgroundColor:"#4c6967"}} className="btn text-white" onClick={(e) => updateClick(e, true)}>Edit your profile</button>
+                <button style={{ backgroundColor: "#4c6967" }} className="btn text-white" onClick={(e) => updateClick(e, true)}>Edit your profile</button>
             </div>
             {showProfile ?
-                <div className="container shadow p-5 mt-3 col-lg-10" style={{ width: "500px", marginTop: "50px", backgroundColor:"#b1d1b4b7"}}>
+                <div className="container shadow p-5 mt-3 col-lg-10" style={{ width: "500px", marginTop: "50px", backgroundColor: "#b1d1b4b7" }}>
                     <label>Change your profile email:</label>
                     <input type="text" value={updateEmail} onChange={(e) => setUpdateEmail(e.target.value)} />
                     <br /><br />
@@ -115,12 +115,12 @@ export default function ProfilePage(props) {
                     <button class="btn btn-block btn-success mt-2 p-2 shadow" onClick={(e) => btnClick(e)}>Build a new trip!</button>
                 </div>
             }
-                
+
             {
                 tripArray.length !== 0 && tripArray.map((trip) => (
                     <div>
-                        <div className="container shadow p-5 mt-3 col-lg-10" style={{ width: "500px", marginTop: "50px", backgroundColor: "#e0e1ccb7"}}>
-                        {trip.pic ?
+                        <div className="container shadow p-5 mt-3 col-lg-10" style={{ width: "500px", marginTop: "50px", backgroundColor: "#e0e1ccb7" }}>
+                            {trip.pic ?
                                 <img src={`https://maps.googleapis.com/maps/api/place/photo?photoreference=${trip.pic}&sensor=false&maxheight=250&maxwidth=250&key=AIzaSyCoiYtN7Xjb7P4JIpWRtlMiL9uQirs_icI`} />
                                 : null}
                             <h3><b>Your Trip to:</b> {trip.destination}</h3>
@@ -130,10 +130,10 @@ export default function ProfilePage(props) {
                             <p><b>Start Location: </b> {trip.startLocation}</p>
                             <p><b>Dates:</b> {getDateRange(trip.dates)}</p>
                             <br />
-                            <button style={{backgroundColor: "#b1d1b4"}} className="btn btn-block mt-2 p-2 shadow" onClick={(e) => removeTrip(e, trip._id)}>Remove Trip</button>
-                            <button style={{backgroundColor: "#69ab8e"}} className="btn btn-block btn-success mt-2 p-2 shadow" onClick={(e) => updateTrip(e, trip._id)}>Update Trip</button>
+                            <button style={{ backgroundColor: "#b1d1b4" }} className="btn btn-block mt-2 p-2 shadow" onClick={(e) => removeTrip(e, trip._id)}>Remove Trip</button>
+                            <button style={{ backgroundColor: "#69ab8e" }} className="btn btn-block btn-success mt-2 p-2 shadow" onClick={(e) => updateTrip(e, trip._id)}>Update Trip</button>
                             <Link to="/itinerary">
-                            <button style={{backgroundColor: "#edd769"}} className="btn btn-block mt-2 p-2 shadow"> Itinerary</button>
+                                <button style={{ backgroundColor: "#edd769" }} className="btn btn-block mt-2 p-2 shadow"> Itinerary</button>
 
                             </Link>
                         </div>
@@ -141,6 +141,6 @@ export default function ProfilePage(props) {
                     </div>
                 ))
             }
-                    </div>
+        </div>
     )
 }
